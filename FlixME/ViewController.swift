808ad73f5e6f02e2330dc8ccdd89527bfb850489
Js <Js@princess-2.local>
2019-02-06 20:58:54 -0800

    Initial Commit

---
+++ FlixME/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixME
+//
+//  Created by Js on 2/6/19.
+//  Copyright © 2019 Zlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FlixMETests/FlixMETests.swift
@@ -0,0 +1,34 @@
+//
+//  FlixMETests.swift
+//  FlixMETests
+//
+//  Created by Js on 2/6/19.
+//  Copyright © 2019 Zlabs. All rights reserved.
+//
+
+import XCTest
+@testable import FlixME
+
+class FlixMETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
